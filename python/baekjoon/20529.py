cb4fb3e11436821fa5d0de1d5da521d3eb6432a7
Lee Hyeokju <hyeokju1313@gmail.com>
2024-05-10 13:36:21 +0900

    Create 20529.py

---
+++ python/baekjoon/20529.py
@@ -0,0 +1,23 @@
+def distance(mbti): # 각 유형별로 비교 해서 다르면 거리를 1씩 증가하는 함수
+    d = 0
+    for i in range(4):
+        d += mbti[0][i] != mbti[1][i]
+        d += mbti[0][i] != mbti[2][i]
+        d += mbti[1][i] != mbti[2][i]
+
+    return d
+
+for _ in range(int(input())): # 테스트 케이스 수(3명)
+    N = int(input()) # 사람 수
+    mbti = input().split() # 각 사람의 mbti
+
+    if N > 32: # 9999집
+        print(0)
+    else:
+        case = 12 # 최대 가능 거리를 default로 먼저 선언
+
+        for i in range(N): # 열심히 비교해라
+            for j in range(i+1, N):
+                for k in range(j+1, N):
+                    case = min(case, distance([mbti[i], mbti[j], mbti[k]])) #min() 함수 사용해서 더 적은 거리를 case로 반영
+        print(case)
