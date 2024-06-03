c26474d2ebdadb683a732aed85266d23db94eba6
Lee Hyeokju <hyeokju1313@gmail.com>
2024-06-03 21:44:47 +0900

    style: pccp_prob3.py

--- python/programmers/pccp_prob3.py
+++ python/programmers/pccp_prob3.py
@@ -8,30 +8,30 @@ def solution(h1, m1, s1, h2, m2, s2):
         answer += 1
 
     while initial_time < final_time:
-        current_hour_angle = initial_time / 120 % 360
-        current_min_angle = initial_time / 10 % 360
-        current_sec_angle = initial_time * 6 % 360
+        current_hour = initial_time / 120 % 360
+        current_min = initial_time / 10 % 360
+        current_sec = initial_time * 6 % 360
 
         if (initial_time + 1) / 120 % 360 == 0:
-            next_hour_angle = 360
+            next_hour = 360
         else:
-            next_hour_angle = (initial_time + 1) / 120 % 360
+            next_hour = (initial_time + 1) / 120 % 360
 
         if (initial_time + 1) / 10 % 360 == 0:
-            next_min_angle = 360
+            next_min = 360
         else:
-            next_min_angle = (initial_time + 1) / 10 % 360
+            next_min = (initial_time + 1) / 10 % 360
 
         if (initial_time + 1) * 6 % 360 == 0:
-            next_sec_angle = 360
+            next_sec = 360
         else:
-            next_sec_angle = (initial_time + 1) * 6 % 360
+            next_sec = (initial_time + 1) * 6 % 360
 
-        if current_sec_angle < current_hour_angle and next_sec_angle >= next_hour_angle:
+        if current_sec < current_hour and next_sec >= next_hour:
             answer += 1
-        if current_sec_angle < current_min_angle and next_sec_angle >= next_min_angle:
+        if current_sec < current_min and next_sec >= next_min:
             answer += 1
-        if next_sec_angle == next_hour_angle and next_hour_angle == next_min_angle:
+        if next_sec == next_hour and next_hour == next_min:
             answer -= 1
 
         initial_time += 1
